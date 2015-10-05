83a4dadd8c9bc471adca3f13c887fa3b972b2c09
Alejandro <alejandro.sanchez.cabana@gmail.com>
2015-10-05 13:23:05 +0200

    Authorization added

--- service/src/main/java/edu/upc/eetac/dsa/beeter/BeeterResourceConfig.java
+++ service/src/main/java/edu/upc/eetac/dsa/beeter/BeeterResourceConfig.java
@@ -1,12 +1,15 @@
 package edu.upc.eetac.dsa.beeter;
 
 import org.glassfish.jersey.server.ResourceConfig;
+import org.glassfish.jersey.server.filter.RolesAllowedDynamicFeature;
 
 /**
  * Created by Alex on 30/9/15.
  */
-public class BeeterResourceConfig extends ResourceConfig{
+public class BeeterResourceConfig extends ResourceConfig {
     public BeeterResourceConfig() {
-        final ResourceConfig rc = new ResourceConfig().packages("edu.upc.eetac.dsa.beeter");
+        packages("edu.upc.eetac.dsa.beeter");
+        packages("edu.upc.eetac.dsa.beeter.auth");
+        register(RolesAllowedDynamicFeature.class);
     }
 }
