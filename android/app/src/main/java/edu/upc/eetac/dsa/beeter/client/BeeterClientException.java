fe32d1ae8d80fde088576cf8d36a93f878b8d016
Alejandro <alejandro.sanchez.cabana@gmail.com>
2016-01-17 11:36:36 +0100

    Bet

---
+++ android/app/src/main/java/edu/upc/eetac/dsa/beeter/client/BeeterClientException.java
@@ -0,0 +1,10 @@
+package edu.upc.eetac.dsa.beeter.client;
+
+/**
+ * Created by Alex on 26/12/15.
+ */
+public class BeeterClientException extends Exception{
+    public BeeterClientException(String detailMessage) {
+        super(detailMessage);
+    }
+}
